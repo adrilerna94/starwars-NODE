c2d99b101174b530dc69a1c5f06d8fb807f50450
adrilerna94 <you@example.comgit config --global user.email you@example.com>
2025-01-12 15:45:39 +0100

    feat: :white_check_mark: api route in app.ts done created principal route /api

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import './loadEnvironment';
+import type { Request, Response } from 'express';
 import express from 'express';
-// import apiRouter from './api/index';
+import apiRouter from './api/index';
 
-// Initialize express
 const app = express();
 
-//app.use('/api/v1', apiRouter);
+app.use('/api', apiRouter);
+app.get ('/ping', (req: Request, res: Response) => res.send('pong'));
 
 export default app;
