a58e2109c4c92e2a8db8ef913787477a62fc61c0
adrilerna94 <you@example.comgit config --global user.email you@example.com>
2025-01-13 00:33:41 +0100

    perf: :sparkles: including starwarsUidSchema to validate request in /:uid endpoint

--- src/api/routers/starWarsRouter.ts
+++ src/api/routers/starWarsRouter.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { getPeopleByNumber } from '../controllers/starWarsController';
+import { validateRequest } from '../middlewares/starWarsRequestValidation';
+import { starwarsUidSchema } from '../../models/starwarsSchemas';
 
 export const starWarsRouter = Router();
 
-starWarsRouter.get('/:uid', getPeopleByNumber);
+starWarsRouter.get('/:uid', validateRequest(starwarsUidSchema, 'params'), getPeopleByNumber);
 
 
-// export default starWarsRouter;
