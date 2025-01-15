1bfc0707655674465c3c443f589f1172509f9156
adrilerna94 <you@example.comgit config --global user.email you@example.com>
2025-01-15 16:07:01 +0100

    feat: :zap: inicialize global var called cache in app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,12 @@ import './loadEnvironment';
 import type { Request, Response } from 'express';
 import express from 'express';
 import apiRouter from './api/index';
+import { StarWars } from './api/interfaces/starwars.interface';
+
+// Inicialización global de cache
+
+global.cache = {} as Record<number, {data: StarWars, expiration: number}>;
+
 
 const app = express();
 
