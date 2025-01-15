2d5c8970a7dd4da7b7608ee51a2ff0034f852db2
adrilerna94 <you@example.comgit config --global user.email you@example.com>
2025-01-15 12:38:38 +0100

    feat: :rocket: starwars interface done

---
+++ src/api/interfaces/starwars.interface.ts
@@ -0,0 +1,25 @@
+
+export interface StarWars {
+  message: string,
+  result: {
+    properties: {
+      height: number,
+      mass: number,
+      hair_color: string,
+      skin_color: string,
+      eye_color: string,
+      birth_year: string,
+      gender: "male" | "female",
+      created: Date,
+      edited: Date,
+      name: string,
+      homeworld: string, //URL -->  type URL ➡️  new URL ("url") aquí no es necesario
+      url: string // URL
+    }
+  },
+  description: string,
+  _id: string,
+  uid: number,
+  _v: number // versión del documento, allways a numner
+
+  }
